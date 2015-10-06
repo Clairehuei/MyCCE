86f11d1a720d114535023b06b2037a071e19e936
Allen <clairehuei1216@gmail.com>
2015-10-07 01:38:39 +0800

    新增 實作按鈕操控surfaceview

--- app/src/main/java/com/codekitchen/allen/mycce/Activity_Surface.java
+++ app/src/main/java/com/codekitchen/allen/mycce/Activity_Surface.java
@@ -1,27 +1,93 @@
 package com.codekitchen.allen.mycce;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SurfaceHolder;
+import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class Activity_Surface extends AppCompatActivity {
+
+//    @InjectView(R.id.btnLeft)
+//    Button btnLeft;
+//
+//    @InjectView(R.id.btnRight)
+//    Button btnRight;
+
+
+//    @InjectView(R.id.olaview)
+//    SurfaceView surfaceView;
+
+    DrawTest vdt;
+
+    Button btnLeft;
+    Button btnRight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        ButterKnife.inject(this);
 
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//隱去標題
+////
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-//        DrawTest drawTest = new DrawTest(this);
         setContentView(R.layout.activity_surface);
+
+        vdt = (DrawTest)findViewById(R.id.olaview);
+        btnLeft = (Button)findViewById(R.id.btnLeft);
+        btnRight = (Button)findViewById(R.id.btnRight);
+
+        btnLeft.setOnClickListener(mOnClickListener1);
+        btnRight.setOnClickListener(mOnClickListener2);
     }
+
+
+    private View.OnClickListener mOnClickListener1 = new View.OnClickListener(){
+        public void onClick(View v){
+            Log.e("btnLeftClick", "*********************************************************");
+            vdt.settLeft(true);
+        }
+    };
+
+    private View.OnClickListener mOnClickListener2 = new View.OnClickListener(){
+        public void onClick(View v){
+            Log.e("btnRightClick", "*********************************************************");
+            vdt.settRight(true);
+        }
+    };
+
+
+
+
+//    @OnClick(R.id.btnLeft)
+//    void btnLeftClick(){
+//        Log.e("btnLeftClick","*********************************************************");
+//        vdt.settLeft(true);
+//    }
+//
+//    @OnClick(R.id.btnRight)
+//    void btnRightClick(){
+//        Log.e("btnLeftClick","<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+//        vdt.settRight(true);
+//    }
+
+
+    @Override
+    protected void onPause (){
+        super.onPause();
+    }
+
+
 
 
 
--- app/src/main/java/com/codekitchen/allen/mycce/DrawTest.java
+++ app/src/main/java/com/codekitchen/allen/mycce/DrawTest.java
@@ -1,97 +1,143 @@
 package com.codekitchen.allen.mycce;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
  * Created by 6193 on 2015/10/6.
  */
-public class DrawTest extends SurfaceView implements SurfaceHolder.Callback {
+public class DrawTest extends SurfaceView implements Runnable ,SurfaceHolder.Callback {
 
-    //呼叫getHolder()方法來取得 SurfaceHolder,並指給 holder
-    SurfaceHolder holder = getHolder();
     Bitmap bp;
-    Canvas canvas;
     int x=50,y=100;  //貼圖在螢幕上的 x,y 座標
-    Thread t1;
+    private Thread gameViewThread;
+    private SurfaceHolder surfaceHolder;
+    private Paint paint;
+    private boolean runFlag = false;
+    public static int screen_width, screen_height;
+    protected Resources resources;
+    private Canvas canvas;
+    public String opt = "asc";
+    private boolean tLeft = false;
+    private boolean tRight = false;
+    private boolean tUp = false;
+    private boolean tDown = false;
 
-    // DrawTest 建構子
+    public void settLeft(boolean tLeft) {
+        this.tLeft = tLeft;
+    }
+
+    public void settRight(boolean tRight) {
+        Log.e("settRight","ppppppppppppppppppppppppp");
+        this.tRight = tRight;
+    }
+
+    public void settUp(boolean tUp) {
+        this.tUp = tUp;
+    }
+
+    public void settDown(boolean tDown) {
+        this.tDown = tDown;
+    }
+
+    // DrawTest 建構子 (自行新增時可使用此建構子)
     public DrawTest(Context context) {
         super(context);
         // TODO Auto-generated constructor stub
-
-
+        init();
     }
 
-    // DrawTest 建構子
+    // DrawTest 建構子 for layout(.xml) 建構使用 ; layout使用<com.codekitchen.allen.mycce.DrawTest> tag時 會自動呼叫此建構子
     public DrawTest(Context context, AttributeSet attrs) {
         super(context, attrs);
         // TODO Auto-generated constructor stub
-        //把這個 class 本身(extends SurfaceView)
-        //透過 holder 的 Callback()方法連結起來
-        //下面這行也可寫成 getHolder().addCallback(this);
-        holder.addCallback(this);
-
-        //設定圖片來源
-        bp = BitmapFactory.decodeResource(getResources(), R.drawable.btn_ball_fire);
+        init();
     }
 
-    private void draw() {
-        //1. 鎖住畫布
-        canvas = holder.lockCanvas();
+    private void init(){
+        //呼叫getHolder()方法來取得 SurfaceHolder,並指給 surfaceHolder
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        resources = getResources();
 
-        //2. 在畫布上貼圖
-        canvas.drawBitmap(bp, x, y, null);
+        paint = new Paint();
+        paint.setAntiAlias(true);
 
-        //3. 解鎖並po出畫布
-        holder.unlockCanvasAndPost(canvas);
+        bp = BitmapFactory.decodeResource(getResources(), R.drawable.btn_ball_fire);
+        setKeepScreenOn(true);
+    }
+
+
+    public void draw(Canvas canvas, Paint paint){
+
+        if(canvas!=null){
+            Log.e("daw.canvas","canvas != null");
+            Log.e("daw.canvas(tLeft)",tLeft+"");
+            Log.e("daw.canvas(tRight)",tRight+"");
+
+            //清除上次繪製的殘留影像
+            canvas.drawColor(Color.BLACK);
+
+            if(tLeft){
+                x = x-10;
+            }else if(tRight){
+                x = x+10;
+            }else if(tUp){
+                y = y-10;
+            }else if(tDown){
+                y = y+10;
+            }
+
+            tLeft = false;
+            tRight = false;
+            tUp = false;
+            tDown = false;
+
+
+            //繪製主圖
+            canvas.drawBitmap(bp, x, y, null);
+
+
+//            //自動來回
+//            if(x<=50){
+//                opt = "asc";
+//                x = x+50;
+//            }else if(x>350){
+//                opt = "desc";
+//                x = x-50;
+//            }else if(x>50 && x<=350){
+//                if(opt.equals("asc")){
+//                    x = x+50;
+//                }else{
+//                    x = x-50;
+//                }
+//            }
+
+        }else{
+            Log.e("daw.canvas","canvas is null");
+        }
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         // TODO Auto-generated method stub
 
-        //在 canvas 畫布上貼圖的三個步驟
-        t1 = new Thread() {
-            public void run() {
-
-                while (true) {
-
-//                    draw();
-
-                    canvas = getHolder().lockCanvas();
-
-                    if(canvas != null){
-                        synchronized (getHolder()) {
-                            canvas.drawBitmap(bp, x, y, null);
-                        }
-                        getHolder().unlockCanvasAndPost(canvas);
-                    }
-
-                    if(x<=50){
-                        x = x+50;
-                    }else if(x>350){
-                        x = x-50;
-                    }
-
-                    try {
-//                        Thread.currentThread().sleep(500);
-                        sleep(200);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            }
-        };
-
-        t1.start();
+        screen_width = getWidth();
+        screen_height = getHeight();
+        runFlag = true;
+        gameViewThread = new Thread(this);
+        gameViewThread.start();
     }
+
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
@@ -101,5 +147,43 @@ public class DrawTest extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         // TODO Auto-generated method stub
+        runFlag = false;
+
+        try {
+            gameViewThread.join();
+        } catch (InterruptedException e) {
+            Log.e("surfaceDestroyed", e+"");
+        }
     }
+
+
+    @Override
+    public void run() {
+        Log.e("runFlag(1)",runFlag+"");
+        while(runFlag){
+            try{
+                Log.e("while","----------------------------------------------------");
+                //1. 鎖住畫布
+                canvas = surfaceHolder.lockCanvas();
+
+                //2. 開始繪圖
+                draw(canvas, paint);
+
+                Thread.sleep(100);
+            }catch (Exception e) {
+                Log.e("Error", "刷新屏幕出错！"+e);
+            }finally{
+                if(canvas!=null){
+                    Log.e("finally","canvas!=null");
+                    //3. 解鎖並po出畫布
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }else{
+                    Log.e("finally","canvas is null");
+                }
+            }
+        }
+        Log.e("runFlag(2)",runFlag+"");
+    }
+
+
 }
